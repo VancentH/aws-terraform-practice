80cd06741cc94464e35454e0c2fb39134df2c364
VancentH <hyde31456@gmail.com>
2023-12-29 17:11:14 +0800

    <feat>

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_route_table" "prod-route-table" {
 
 
 # 4. create a subnet
-resource "aws_subnet" "my-subnet" {
+resource "aws_subnet" "prod-subnet" {
   vpc_id            = aws_vpc.prod-vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1b" # 可用區域
@@ -56,6 +56,76 @@ resource "aws_route_table_association" "a" {
 
 
 # 6. create security group to allow port 22,80,443
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow Web inbound traffic"
+  vpc_id      = aws_vpc.prod-vpc.id
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # 定義那些 ip 可以通過，因為這邊是 web server 要讓其他人可以連進來
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # 定義那些 ip 可以通過，因為這邊是 web server 要讓其他人可以連進來
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # 定義那些 ip 可以通過，因為這邊是 web server 要讓其他人可以連進來
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
 # 7. create a network interface with an ip in the subnet that was created in step4
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id       = aws_subnet.prod-subnet.id
+  private_ips     = ["10.0.1.50"] # within subnet cidr_block
+  security_groups = [aws_security_group.allow_web.id]
+}
+
+
 # 8. assign an elastic ip to the network interface created in step7
+# public ip
+# depends on IGW
+resource "aws_eip" "one" {
+  domain                    = "vpc"
+  network_interface         = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on                = [aws_internet_gateway.gw]
+}
+
 # 9. create Ubuntu server and install/enable apache2
+resource "aws_instance" "web-server-aws_instance" {
+  ami               = ""
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1b"
+  key_name          = "main-key"
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.web-server-nic.id
+  }
+
+  # user_data = <<-EOF
+}
